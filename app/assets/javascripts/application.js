8e49a0d2a04c6e86cf302bf1d6cc5940b6a40db7
Bastian Schnieders <bastian.schnieders@me.com>
2017-03-23 18:09:32 +0100

    working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,13 +16,18 @@
 //= require turbolinks
 //= require_tree .
 
-$(function(){
+$(document).on("turbolinks:load ready", function(){
   $(document).foundation();
-  var container = document.getElementById('network');
-  var network = new vis.Network(
-    container,
-    {nodes: gon.networkData.nodes, edges: gon.networkData.edges},
-    gon.networkData.options
-  );
+  initNetwork();
+  
+  function initNetwork(){
+    var container = document.getElementById('network');
+    var network = new vis.Network(
+      container,
+      {nodes: gon.networkData.nodes, edges: gon.networkData.edges},
+      gon.networkData.options
+    );
+  }
+
 
 });
