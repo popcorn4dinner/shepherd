75b030429cc182dad1feb3381383daa0364b8e6b
Bastian Schnieders <bastian.schnieders@stepstone.be>
2018-01-08 15:56:02 +0100

    corrected typo

--- app/models/service_file_reader.rb
+++ app/models/service_file_reader.rb
@@ -81,7 +81,7 @@ class ServiceFileReader
       YAML::load(File.open(File.join( folder, file_name))).deep_symbolize_keys!
     rescue
       raise ServiceConfigurationError,
-            "Unable to read service configuration file. Check the following file #{File.join( folder, file_name)}"
+            "Unable to read service configuration file. Check the following file #{File.join( folder, file_name)}."
     end
   end
 
