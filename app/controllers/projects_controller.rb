6291c51ad9b5f2e34577e7ea782719a05b7c81a8
Bastian Schnieders <bastian.schnieders@stepstone.be>
2018-01-23 13:18:39 +0100

    automatic rubocop improvments

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,36 +1,36 @@
+# frozen_string_literal: true
+
 class ProjectsController < ApplicationController
   before_action :set_projects
-  before_action :set_project, only: [:show, :health]
-  before_action :set_projects
+  before_action :set_project, only: %i[show health]
 
   # GET /projects
   # GET /projects.json
   def index
-    network = NetworkBuilders::ProjectsNetworkBuilder.new(@projects).build()
+    network = NetworkBuilders::ProjectsNetworkBuilder.new(@projects).build
     gon.networkData = network.to_hash
   end
 
   # GET /projects/1
   # GET /projects/1.json
   def show
-    network = NetworkBuilders::ProjectNetworkBuilder.new(@project).build()
+    network = NetworkBuilders::ProjectNetworkBuilder.new(@project).build
     gon.networkData = network.to_hash
   end
 
   def health
-    network = NetworkBuilders::HealthNetworkBuilder.new(@project).build()
+    network = NetworkBuilders::HealthNetworkBuilder.new(@project).build
     gon.networkData = network.to_hash
   end
 
-
-
   private
-    def set_project
-      @project = Project.find_by(slug: params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def project_params
-      params.fetch(:project, {})
-    end
+  def set_project
+    @project = Project.find_by(slug: params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def project_params
+    params.fetch(:project, {})
+  end
 end
--- app/models/verification_runners/health_verification_runner.rb
+++ app/models/verification_runners/health_verification_runner.rb
@@ -1,14 +1,14 @@
+# frozen_string_literal: true
+
 module VerificationRunners
   class HealthVerificationRunner
-
     def self.run(verifier)
       response = RestClient.get verifier.service.health_endpoint
-      return response.code == 200
+      response.code == 200
     end
 
     def self.required_parameters
       []
     end
-
   end
 end
